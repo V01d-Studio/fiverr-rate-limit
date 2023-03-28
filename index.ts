49fb1cc2db9256dc386e792446668de3d1cfc3ca
sihan-tawsik <sihantawsik@gmail.com>
2023-03-28 22:03:38 +0600

    Add mongoose connection

--- index.ts
+++ index.ts
@@ -2,18 +2,19 @@ import express, { Application, NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import rateLimit from "express-rate-limit";
+import mongoose from "mongoose";
 
-import prisma from "./configs/prisma.config";
+// import prisma from "./configs/prisma.config";
 import { wrappedResponse } from "./utils/functions";
 import demoRoute from "./routes/demo.route";
-import { authFactory, AuthSchemes } from "./auth";
+// import { authFactory, AuthSchemes } from "./auth";
 
 dotenv.config();
 
 const app: Application = express();
 const port: number = parseInt(process.env.PORT || "8000");
-const jwtProduct = authFactory(AuthSchemes.JWT);
-const httpProduct = authFactory(AuthSchemes.HTTP);
+// const jwtProduct = authFactory(AuthSchemes.JWT);
+// const httpProduct = authFactory(AuthSchemes.HTTP);
 
 app.use(express.json());
 app.use(
@@ -24,15 +25,15 @@ app.use(
 
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 15 minutes
-  max: 1, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  max: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: { statusCode: 429, message: "Too many requests", result: null },
 });
 
 app.use(limiter);
-app.use("/jwt", jwtProduct.router);
-app.use("/http-token", httpProduct.router);
+// app.use("/jwt", jwtProduct.router);
+// app.use("/http-token", httpProduct.router);
 
 app.use("/hello", demoRoute);
 
@@ -51,12 +52,14 @@ app.use(function onError(
 });
 
 const server = app.listen(port, async () => {
-  await prisma.$connect();
+  // await prisma.$connect();
+  await mongoose.connect(process.env.DATABASE_URL!);
   console.log(`⚡️[server]: Server is running on PORT ${port}`);
 });
 
 process.on("SIGINT", async () => {
-  await prisma.$disconnect();
+  // await prisma.$disconnect();
+  await mongoose.disconnect();
   server.close();
   console.log("[server]: Server closed on SIGINT");
 });
