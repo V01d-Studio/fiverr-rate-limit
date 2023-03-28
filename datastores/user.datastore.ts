011f3dc7a09dec1da2ecb21e971bec7c4bf3af2e
sihan-tawsik <sihantawsik@gmail.com>
2023-03-28 22:33:40 +0600

    Add mongoose user datastore

--- datastores/user.datastore.ts
+++ datastores/user.datastore.ts
@@ -1,13 +1,16 @@
 import { User } from "@prisma/client";
 
 import prisma from "../configs/prisma.config";
+import UserModel from "../models/user.model";
 
 export const findUserById = (id: string) => {
-  return prisma.user.findUnique({ where: { id } });
+  return UserModel.findById(id);
+  // return prisma.user.findUnique({ where: { id } });
 };
 
 export const findUserByEmail = (email: string) => {
-  return prisma.user.findUnique({ where: { email } });
+  return UserModel.findOne({ email }).exec();
+  // return prisma.user.findUnique({ where: { email } });
 };
 
 export const createUser = (user: Omit<User, "id">) => {
--- models/user.model.ts
+++ models/user.model.ts
@@ -16,3 +16,5 @@ const userSchema = new Schema<IUser>({
 
 // 3. Create a Model.
 const User = model<IUser>("User", userSchema);
+
+export default User;
