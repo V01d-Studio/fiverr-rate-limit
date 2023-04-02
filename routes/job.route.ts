64ed0b85954403d1f9aa7c0058a862383958c22f
sihan-tawsik <sihantawsik@gmail.com>
2023-04-02 12:25:06 +0600

    Remove unnecessary code

--- routes/job.route.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { methodNotAllowed } from "../utils/functions";
-import { categorize } from "../controllers/job.controller";
-
-const router = express.Router();
-
-router.route("/").get(categorize).all(methodNotAllowed);
-
-export default router;
